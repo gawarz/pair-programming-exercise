922c173c646d60f6fedaa4cffef09f15d9c9a195
Michal Gawarzynski <michal.gawarzynski@allegro.pl>
2021-08-11 14:06:35 +0200

    parser

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,5 +2,30 @@ fun main(args: Array<String>) {
     val filePath = args[0]
     val reader = FileReader()
     val lines = reader.readLines(filePath)
-    println(lines.toString())
+
+    val entrySource = EntryParser().parseLine(lines)
+
+    println(entrySource)
 }
+
+
+data class EntrySource(val subList: List<String>) {
+
+    init {
+        require(subList.size == 3) { "only 3 lines" }
+        subList.forEach {
+            require(it.length == 27) { "only 27 " }
+        }
+    }
+
+
+}
+
+class EntryParser {
+
+    fun parseLine(lines: List<String>): List<EntrySource> {
+        return lines.chunked(4).filter { it.size == 4 } .map { EntrySource(it.subList(0, 3)) }
+
+    }
+}
+
